f1db7f705cc15c329c11c24e5b94c1e134a2e83e
Madison Carr <madison.carr@utexas.edu>
2020-06-12 09:30:11 -0500

    adds Controller and initializer

---
+++ src/main/java/com/okta/developer/jugtours/web/GroupController.java
@@ -0,0 +1,61 @@
+package com.okta.developer.jugtours.web;
+
+import com.okta.developer.jugtours.model.Group;
+import com.okta.developer.jugtours.model.GroupRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController //combo of Controller and response body, returns the object that is rest api compatible
+@RequestMapping("/api") //directing the request to the right place: ex localhost:8080/api/blah blah blah
+class GroupController {
+
+    private final Logger log = LoggerFactory.getLogger(GroupController.class);
+    private GroupRepository groupRepository;
+
+    public GroupController(GroupRepository groupRepository) {
+        this.groupRepository = groupRepository;
+    }
+
+    @GetMapping("/groups")
+    Collection<Group> groups() {
+        return groupRepository.findAll();
+    }
+    //TODO: Test out <?> vs <T> vs <Group> by putting an id that doesn't exist and see what happens; why use <?> ?
+    @GetMapping("/group/{id}")
+    ResponseEntity<?> getGroup(@PathVariable Long id) { //<?> generic type of unknown, ie wild card
+        Optional<Group> group = groupRepository.findById(id);
+        return group.map(response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/group")
+    ResponseEntity<Group> createGroup(@Valid @RequestBody Group group) throws URISyntaxException {
+        log.info("Request to create group: {}", group);
+        Group result = groupRepository.save(group);
+        return ResponseEntity.created(new URI("/api/group/" + result.getId()))
+                .body(result);
+    }
+
+    @PutMapping("/group/{id}")
+    ResponseEntity<Group> updateGroup(@Valid @RequestBody Group group) {
+        log.info("Request to update group: {}", group);
+        Group result = groupRepository.save(group);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @DeleteMapping("/group/{id}")
+    public ResponseEntity<?> deleteGroup(@PathVariable Long id) {
+        log.info("Request to delete group: {}", id);
+        groupRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+}
