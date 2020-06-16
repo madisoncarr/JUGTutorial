41d2a6afe2539e5ebba9d76244ad4b23eade22cb
Madison Carr <madison.carr@utexas.edu>
2020-06-16 11:10:10 -0500

    adds frontend

---
+++ app/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import GroupList from './GroupList';
+import GroupEdit from './GroupEdit';
+
+class App extends Component {
+
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route path='/' exact={true} component={Home}/>
+                    <Route path='/groups' exact={true} component={GroupList}/>
+                    <Route path='/groups/:id' component={GroupEdit}/>
+                </Switch>
+            </Router>
+        )
+    }
+}
+
+export default App;
