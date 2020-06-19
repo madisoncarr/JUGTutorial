d577830eb614c61c894fa3d52f652b043e4e4bbb
Matti Barzilai <matti.barzilai@gmail.com>
2020-06-19 09:29:01 -0500

    finish adding user functionality to GroupController, start adding auth to frontend

--- src/main/java/com/okta/developer/jugtours/model/UserRepository.java
+++ src/main/java/com/okta/developer/jugtours/model/UserRepository.java
@@ -1,4 +1,6 @@
 package com.okta.developer.jugtours.model;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/com/okta/developer/jugtours/web/GroupController.java
+++ src/main/java/com/okta/developer/jugtours/web/GroupController.java
@@ -2,17 +2,22 @@ package com.okta.developer.jugtours.web;
 
 import com.okta.developer.jugtours.model.Group;
 import com.okta.developer.jugtours.model.GroupRepository;
+import com.okta.developer.jugtours.model.User;
 import com.okta.developer.jugtours.model.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.security.Principal;
 import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController //combo of Controller and response body, returns the object that is rest api compatible
@@ -29,8 +34,8 @@ class GroupController {
     }
 
     @GetMapping("/groups")
-    Collection<Group> groups() {
-        return groupRepository.findAll();
+    Collection<Group> groups(Principal principal) {
+        return groupRepository.findAllByUserId(principal.getName());
     }
 
     //TODO: Test out <?> vs <T> vs <Group> by putting an id that doesn't exist and see what happens; why use <?> ?
@@ -42,8 +47,17 @@ class GroupController {
     }
 
     @PostMapping("/group")
-    ResponseEntity<Group> createGroup(@Valid @RequestBody Group group) throws URISyntaxException {
+    ResponseEntity<Group> createGroup(@Valid @RequestBody Group group,
+                                      @AuthenticationPrincipal OAuth2User principal) throws URISyntaxException {
         log.info("Request to create group: {}", group);
+        Map<String, Object> details = principal.getAttributes(); //what attributes does the principal have?
+        String userId = details.get("sub").toString();
+
+        // check to see if user already exists
+        Optional<User> user = userRepository.findById(Long.parseLong(userId));
+        group.setUser(user.orElse(new User(userId,
+                details.get("name").toString(), details.get("email").toString())));
+
         Group result = groupRepository.save(group);
         return ResponseEntity.created(new URI("/api/group/" + result.getId())) //created http status and location
                 .body(result);
